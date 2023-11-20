e437f46eb0147096594f6ac86a1b02cf0633dcca
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-11-20 18:09:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var people = 0;
+var tries = 0;
+app.use(express.static(__dirname+"/public"))
+
+io.on('connection', (socket) => {
+	people++;
+	
+	socket.nickname = people;
+	if(people> 1){
+		(async ()=>{
+			var sockets = await io.fetchSockets();
+			sockets.forEach(sock=> {
+				sock.broadcast.emit("joined", people);
+			})
+		})();
+	}
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('a user disconnected')
+  })
+	socket.on("video", v=> {
+		if(people> 1){
+			(async ()=>{
+				var sockets = await io.fetchSockets();
+				sockets.forEach(sock=> {
+					sock.broadcast.emit("v", {video: v, number: socket.nickname});
+				})
+			})();
+		}
+	})
+});
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
